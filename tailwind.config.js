62f4ee1bc703953ed2e87be9916815bc57a8d907
Abdul-Muqaddam <chandansar444999@gmail.com>
2024-11-22 15:35:45 +0500

    server configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./Actual_code/**/*.{html,js}"],
-  theme: {
-    extend: {
-      screens:{
-        "mq-950":{max:"950px"},
-        "mq-750":{max:"750px"},
-        "mq-1050":{max:"1050px"},
-        "mq-500":{max:"500px"},
-        "mq-600":{max:"600px"},
-        "mq-1130":{max:"1130px"},
-        "mq-1060":{max:"1060px"},
-        "mq-360":{max:"360px"}
-      }
-    },
+export const content = ["./Actual_code/**/*.{html,js}"];
+export const theme = {
+  extend: {
+    screens: {
+      "mq-950": { max: "950px" },
+      "mq-750": { max: "750px" },
+      "mq-1050": { max: "1050px" },
+      "mq-500": { max: "500px" },
+      "mq-600": { max: "600px" },
+      "mq-1130": { max: "1130px" },
+      "mq-1060": { max: "1060px" },
+      "mq-360": { max: "360px" }
+    }
   },
-  plugins: [],
-}
+};
+export const plugins = [];
 
